a70147708ac6cf8c242a4a3d1c81b8a00b5d5d46
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-11 20:05:44 +0900

    Added Tailwind support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './input.css';
+import '/dist/output.css';
+
+const test = document.createElement('p');
+test.textContent = 'Test';
+test.classList.add('text-red-500');
+document.body.appendChild(test);
