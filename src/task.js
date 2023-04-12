d09a2962789a005a5025f1949bd297a7b988c5c3
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-12 15:24:24 +0900

    Begun creating logic

---
+++ src/task.js
@@ -0,0 +1,16 @@
+import '/dist/output.css';
+
+// TODO Create Create button
+
+// Creates task item for the right area of the UI
+const item = (title, description, dueDate, priority) => {
+
+};
+
+// Create form that sends info to the "item" factory
+const form = (() => {
+
+  const form = document.createElement('form');
+  
+
+})();
